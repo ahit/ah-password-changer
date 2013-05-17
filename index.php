49acbfa55f02ee5f42ea838f807c478cf3733e89
lkozloff <lyle@asianhope.org>
2013-05-17 14:54:12 +0700

    adding code to update database and disclose the password to teachers

--- index.php
+++ index.php
@@ -2,17 +2,28 @@
 <html>
 <head>
     <link rel = "stylesheet" type="text/css" href="style.css" media="screen"/>
+    <script type ="text/javascript" src="checkpass.js"></script>
 </head>
 <body>
-<form>
-<h1>username:</h1>
-<input type = "text"><br/><br/>
-<h1>choose a new password:</h1>
-<h2>don't forget to write it down</h2>
-<ul>
+
+<?php
+session_start();
+if(isset($_SESSION['generated_pws']))
+    $generated_pws = $_SESSION['generated_pws'];
+else
+    $generated_pws = Array();
+
+
+//case first time or want to regenerate passwords
+if(!empty($_POST['regen']) || empty($_POST['submit']) || (empty($_POST['submit']) && empty($_POST['new_pass']))){?>
+<form action = "index.php" method = "post">
+<h1>your username:</h1>
+<input type = "text" name = "username"><br/><br/>
+<h1>here are some new password options:</h1>
 
 <?php
     include('pwgen.class.php');
+
     $length = 8;
     $secure = false;
     $numerals = true;
@@ -21,23 +32,93 @@
     $no_vowels = false;
     $symbols = true;
     $pwgen = new PWGen($length,$secure,$numerals,$capitalize,$ambiguous,$no_vowels,$symbols);
-    for($i = 0; $i<10; $i++){
+
+    if(isset($_SESSION['i']) && !empty($_SESSION['i']))$i = $_SESSION['i'];
+    else { $i = 0; $_SESSION['i'] = 0;}
+
+    for($i; $i<$_SESSION['i']+10; $i++){
         $password = $pwgen->generate();
         if(strlen($password)==$length)
-        echo "<li>".$password."</li>";
+        echo "<div class = \"new_pass\"><input type =\"radio\" name = \"pass_id\" value =\"$i\">$password</input></div>\n";
+        $generated_pws[$i] = $password;
     }
+
+    $_SESSION['i'] = $i;
+    $_SESSION['generated_pws'] = $generated_pws;
 
 
 ?>
-</ul>
-<input type ="submit" value = "I don't like these ones"/><br/>
-<h1>now, practice it 3 times:</h1>
-<input type ="password">
-<input type ="password">
-<input type ="password">
-<h1>and just to make sure you're you, type your old password:</h1>
-<input type ="password"><br/><br/>
-<input type ="submit" value = "Submit"/>
+<input type ="submit" name = "regen" value = "I don't like these ones"/><br/>
+<h1>now, practice your favorite one 3 times:</h1>
+<h2>(this is optional - if you put nothing here, I won't tell)</h2>
+<input type ="password" id="pass1">
+<input type ="password" id ="pass2" onkeyup="checkPass(); return false;">
+<input type ="password" id ="pass3" onkeyup="checkPass(); return false;">
+<h1>and just to make sure you're you, type your opensis password:</h1>
+<input type ="password" name ="password"><br/><br/>
+<input type ="submit" name = "submit" value = "Submit"/>
 </form>
+<?php
+}//end initial view
+else{
+    include('db.php');
+
+    if(empty($_SESSION['attempts'])) $_SESSION['attempts'] = 1;
+    else $_SESSION['attempts']++;
+
+    $pass_id = $_POST['pass_id'];
+    $new_pass = $generated_pws[$pass_id];
+    $old_pass = $_POST['password'];
+    $username = $_POST['username'];
+
+    $dbh = connectDB();
+
+    $sql = "SELECT student_id as sid FROM students WHERE username = '$username' AND password = '".md5($old_pass)."'";
+    $query = $dbh->prepare($sql);
+    $query->execute();
+    $res = $query->fetch();
+    $sid = $res['sid'];
+
+    if(!empty($sid)){ //that is, if res is non-zero
+        echo "<h4>you changed your password!</h4>";
+        echo "<p>Don't forget it: <div class = \"new_pass\">$new_pass</div></p>";
+        //actually change the password and update student records
+
+        $sql = "UPDATE students set password = '".md5($new_pass)."' WHERE username = '$username'";
+        $query = $dbh->prepare($sql);
+        $query->execute();
+
+        $sql = "INSERT INTO student_mp_comments values ('','$sid','2012','0','31','Student updated password to: $new_pass','".date("Y-m-d")."')";
+        $query = $dbh->prepare($sql);
+        $query->execute();
+
+        session_destroy();
+    }
+    else if($_SESSION['attempts']<3){
+        echo "<h3>incorrect username or password</h3>";
+        echo "<h1>on attempt: ".($_SESSION['attempts']+1)."/3</h1>";
+        echo "<h2>you selected password: </h2><div class = \"new_pass\">$new_pass</div>";
+    ?>
+    <form action = "index.php" method = "post">
+    <input type = "hidden" name = "pass_id" value = "<?php echo $pass_id;?>">
+    <h1>username:</h1>
+    <input type = "text" name = "username"/>
+    <h1>password</h1>
+    <input type = "password" name = "password"/>
+    <input type = "submit" value = "Submit" name = "submit">
+
+    </form>
+    <?php
+    }
+    else {
+
+        echo "Sorry, bro. <a href = \"index.php\">Try again?</a>";
+
+
+        session_destroy();
+    }
+}
+?>
+
 </body>
 </html>
